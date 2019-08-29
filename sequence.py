9dc1f453142a7bd44ab956a33fc2f362e6679a45
cybaj <cybaj@naver.com>
2019-08-30 00:26:43 +0900

    add nsteps;

--- sequence.py
+++ sequence.py
@@ -1,6 +1,8 @@
 '''
 sequence modules
 '''
+import math
+
 def get_zero_matrix(i, j):
     '''
     returns zeros 2d matrix.
@@ -28,4 +30,37 @@ def odds(seq):
     '''
     for i in range(1, ((len(seq) - 1) // 2)):
         seq.pop(i)
+    return seq
+
+def triples(seq):
+    '''
+    returns sequence of triple members.
+    '''
+    lmt = math.ceil((len(seq) - len(seq) // 3) / 2)
+    rmd = (len(seq) % 3)
+    for i in range(0, lmt):
+        seq.pop(i)
+        if lmt - 1 == i and not rmd == 0:
+            for _ in range(1, rmd):
+                seq.pop(i)
+        else:
+            seq.pop(i)
+    return seq
+
+def n_steps(n_s, seq):
+    '''
+    returns sequence of n-step members.
+    '''
+    lmt = math.ceil((len(seq) - len(seq) // n_s) / (n_s - 1))
+    rmd = (len(seq) % n_s)
+    print(lmt)
+    print(rmd)
+    for i in range(0, lmt):
+        seq.pop(i)
+        if lmt - 1 == i and not rmd == 0:
+            for _ in range(1, rmd):
+                seq.pop(i)
+        else:
+            for _ in range(n_s - 2):
+                seq.pop(i)
     return seq
